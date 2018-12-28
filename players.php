fdb56682b59d2d3248251f0700b56a491dfc1b62
bubbajdf <38475258+bubbajdf@users.noreply.github.com>
2018-12-28 13:14:59 -0500

    Update players.php

--- players.php
+++ players.php
@@ -3,6 +3,7 @@
 
 <script type="text/javascript" src="jquery/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="js/players.js"></script>
+<link rel="stylesheet" type="text/css" href="css\players.css">
 <h1 align="center">Executive Decision Players</h1>
 
 <div>
@@ -11,25 +12,10 @@
 <br>
 <label for="player2">Player 2 Name:</label>
 <input type="text" name="player2" id="player2" value="">
-<button id="add2" onclick="AddPlayerClick(this)">Add Player</button>
-<button id="FamilyID_PVSearchButton">Delete Player</button>
+<input type="button" id="add2" onclick="AddPlayerClick(this)" value="Add Player">
 </div>
 <div id="extraplayers" name="extraplayers">
 </div>
-<!--
-<br>
-<label for="player3">Player 3 Name:</label>
-<input type="text" name="player3" id="player3" value="">
-<br>
-<label for="player4">Player 4 Name:</label>
-<input type="text" name="player4" id="player4" value="">
-<br>
-<label for="player5">Player 5 Name:</label>
-<input type="text" name="player5" id="player5" value="">
-<br>
-<label for="player6">Player 6 Name:</label>
-<input type="text" name="player6" id="player6" value="">
--->
 <br>
 <input type="submit" name="submit" id="submit" value="Submit" style="display:block">
 <br>
