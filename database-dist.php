40cf8cc0f698b519cddefc069ed058651c4f7c75
bubbajdf <38475258+bubbajdf@users.noreply.github.com>
2018-12-28 11:34:17 -0500

    Add files via upload

---
+++ database-dist.php
@@ -0,0 +1,12 @@
+<?php
+
+//*************************************************************************************************
+// MYSQL DATABASE CONNECTION:
+// Replace the values inside the single quotes below with the values for your MySQL configuration. 
+// If not using the default port 3306, then append a colon and port number to the hostname
+// (e.g. $hostname = 'example.com:3307';).
+
+$hostname 	= '';
+$db 		= '';
+$username 	= '';
+$password 	= '';
