8a6abfef3f504222f835093fa3536b0cdecf5bd7
bubbajdf <38475258+bubbajdf@users.noreply.github.com>
2019-01-09 10:42:37 -0500

    Add files via upload

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+
+define('EXEC_DECISION_ROOT', __DIR__ . '/');
+define('EXEC_DECISION_CLASSES', EXEC_DECISION_ROOT . 'classes/');
+define('EXEC_DECISION_CONTROLLERS', EXEC_DECISION_ROOT . 'controllers/');
