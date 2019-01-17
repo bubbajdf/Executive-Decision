9fcd57733ac472e707e5be9f7060b47f93aad261
bubbajdf <38475258+bubbajdf@users.noreply.github.com>
2019-01-17 15:19:55 -0500

    Update tally_card.php

--- tally_card.php
+++ tally_card.php
@@ -1,5 +1,5 @@
 <?php
-require_once EXEC_DECISION_CLASSES . 'Price-Board-Utils.php';
+require_once EXEC_DECISION_CLASSES . 'TallyCardUtils.php';
 ?>
 
 <h1 align="center">Executive Decision Tally</h1>
@@ -13,113 +13,59 @@ require_once EXEC_DECISION_CLASSES . 'Price-Board-Utils.php';
     <th class="color" colspan="19">ORDERS</th>
   </tr>
 <!-- JAN THRU JUNE -->
-  <tr>
-   <th></th>
-   <th colspan="3">JANUARY</th>
-   <th colspan="3">FEBRUARY</th>
-   <th colspan="3">MARCH</th>
-   <th colspan="3">APRIL</th>
-   <th colspan="3">MAY</th>
-   <th colspan="3">JUNE</th>
-  </tr>
-  <tr>
-   <th>Raw Materials</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
- </tr>
+<?php
+TallyCardUtils::CreateJANJUNOrderHeaders();
+?>
  <tr>
    <td>X-FINE</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <td>FINE</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <td>STD.</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <th></th>
 <?php
-PriceBoardUtils::CreateBlankTotals();
+TallyCardUtils::CreateBlankTotals();
 ?>
  </tr>
 <!-- JULY THRU DEC -->
-  <tr>
-   <th></th>
-   <th colspan="3">JULY</th>
-   <th colspan="3">AUGUST</th>
-   <th colspan="3">SEPTEMBER</th>
-   <th colspan="3">OCTOBER></th>
-   <th colspan="3">NOVEMBER</th>
-   <th colspan="3">DECEMBER</th>
-  </tr>
-  <tr>
-   <th>Raw Materials</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Paid</th>
- </tr>
+<?php
+TallyCardUtils::CreateJULDECOrderHeaders();
+?>
  <tr>
    <td>X-FINE</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <td>FINE</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <td>STD.</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
  </tr>
  <tr>
    <th></th>
 <?php
-PriceBoardUtils::CreateBlankTotals();
+TallyCardUtils::CreateBlankTotals();
 ?>
  </tr>
 </table>
@@ -129,111 +75,58 @@ PriceBoardUtils::CreateBlankTotals();
     <th class="color" colspan="19">SALES</th>
   </tr>
 <!-- JAN THRU JUNE -->
-  <tr>
-   <th></th>
-   <th colspan="3">JANUARY</th>
-   <th colspan="3">FEBRUARY</th>
-   <th colspan="3">MARCH</th>
-   <th colspan="3">APRIL</th>
-   <th colspan="3">MAY</th>
-   <th colspan="3">JUNE</th>
-  <tr>
-  <tr>
-   <th>Finished Goods</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-  <tr>
+<?php
+TallyCardUtils::CreateJANJUNSalesHeaders();
+?>
     <td>A</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
   <tr>
     <td>B</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
   <tr>
     <td>C</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
  <tr>
    <th></th>
 <?php
-PriceBoardUtils::CreateBlankTotals();
+TallyCardUtils::CreateBlankTotals();
 ?>
  </tr>
 <!-- JULY THRU DEC -->
-  <tr>
-   <th></th>
-   <th colspan="3">JULY</th>
-   <th colspan="3">AUGUST</th>
-   <th colspan="3">SEPTEMBER</th>
-   <th colspan="3">OCTOBER</th>
-   <th colspan="3">NOVEMBER</th>
-   <th colspan="3">DECEMBER</th>
-  </tr>
-  <tr>
-   <th>Finished Goods</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
-   <th>Units Bid</th>
-   <th>Bid Price</th>
-   <th>Amount Rcvd.</th>
+<?php
+TallyCardUtils::CreateJULDECSalesHeaders();
+?>
   <tr>
     <td>A</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
   <tr>
     <td>B</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
   <tr>
     <td>C</td>
 <?php
-PriceBoardUtils::CreateBlankColumns();
+TallyCardUtils::CreateBlankColumns();
 ?>
   </tr>
  <tr>
    <th></th>
 <?php
-PriceBoardUtils::CreateBlankTotals();
+TallyCardUtils::CreateBlankTotals();
 ?>
  </tr>
 </table>
