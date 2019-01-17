f9f3f17f9ad5f7bd972020d79e5d974fac87028d
bubbajdf <38475258+bubbajdf@users.noreply.github.com>
2019-01-17 15:20:28 -0500

    Add files via upload

---
+++ instructions.php
@@ -0,0 +1,303 @@
+<head>
+<link rel="stylesheet" type="text/css" href="jquery\jquery-ui-1.12.1\jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css\instructions.css">
+<script type="text/javascript" src="jquery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="jquery/jquery-ui-1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="js/instructions.js"></script>
+</head>
+
+<div class="accordion">
+<h2>SETUP</h2>
+<div>
+Players decide how many months will consitute a game.  Generally a game consists
+of 12 months, however, beginners may prefer a shorter game until they become
+familar with the play.
+<br><br>
+Each player receives a Tally sheet and the following amount of money:
+<ul>
+<li>2 players: $900</li>
+<li>3 players: $600</li>
+<li>4 players: $450</li>
+<li>5 players: $350</li>
+<li>6 players: $300</li>
+</ul>
+One player is chosen to be the Banker throughout the game.  That player handles
+the Raw Material Certificates and money and posts the current Market Prices on the
+Price Level Board.
+</div>
+<h2>PLAY</h2>
+<div>
+Each month of play consists of:
+<ol>
+<li>Purchasing Raw Materials, which are used to</li>
+<li>Manufactoring the products of your choice and</li>
+<li>Selling the Finished Goods in an attempt to make the greatest profit for your corporation.</li>
+</ol>
+<b>MONTH 1 (January) consists of the following, in order:</b><br><br>
+<div class="accordion">
+<h3>1. PURCHASING RAW MATERIAL CERTIFICATES.</h3>
+<div>
+After consulting the Price Level Board
+(<i>Opening Price</i> column and <i>Market Calculator</i>), each player enters his January Order
+for Raw Materials in the Big columns on the Tally Sheet - secretly recording
+how many units of each grade he wishes to purchase and the <i>price per unit</i> he is
+willing to pay.
+
+Players orders may include bids for any or all of the three different grades of
+Materials, but the total units of <i>all grades</i> ordered by one player <i>may not
+exceed</i> the following amounts:
+<br><br>
+<table>
+  <th>Number of Players</th>
+  <th>Total Per Players</th>
+  <tr>
+    <td>2</td>
+    <td>18 units*</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>12 units</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td>9 units</td>
+  </tr>
+  <tr>
+    <td>5</td>
+    <td>7 units</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>6 units</td>
+  </tr>
+</table>
+<span class="smalllabel">* When two play, no player may order more then 12 units of one grade of Material.</span>
+<br><br>
+When all bids have been recorded, each player, <i>in turn</i>, announces how many units
+of X-FINE Material he has ordered.  Broker totals annouced units and consults
+Market Calculator (Raw Materials Section) for price change.  Amount shown on
+Calculator is added to or subtracted from Opening Price and resulting Market
+Price for X-FINE is posted in January column of Price Level Board.  Market
+Prices for FINE and STANDARD Materials are then determined in the same manner.
+<br><br>
+<i>Example: In January, orders of X-FINE total 12 units.  Market Calculator shows
++2 which is added to Opening Price of $40.  January Market Price of X-FINE is $42.
+Players also ordered 5 units of FINE and 0 units of STANDARD.  Calculator shows
+-5 for FINE and -10 for STANDARD.  January prices for these Materials are $25
+and $10 respectively.</i>
+<br><br>
+Raw Material prices never drop below $1 per unit.  If Market Calculator indicates
+that a particular Raw Material will drop to $0 or below, the Market Price of that
+Material is <i>stabilized at $1</i> until demand forces the price up again.
+<br><br>
+After the current Market Prices have been posted, each player determines which
+Raw Materials he has been successful in purchasing according to the following chart:
+<br><table>
+ <th>PLAYER's BID</th>
+ <th>PLAYER's PURCHASE</th>
+ <tr>
+   <td>Equal to or higher than Market Price</td>
+   <td>All Units at Bid Price</td>
+ </tr>
+ <tr>
+   <td>Lower than Market Price</td>
+   <td>No Units</td>
+ </tr>
+</table>
+After adjusting his Tally Sheet by <i>crossing out any unsuccessful bids</i>, each player
+totals his purchases, pays the Broker and receives one Raw Material Certificate
+for each unit purchased.  These Certificates are placed face up in front of player so
+all other players may see them.
+<br><br>
+If a player does not have sufficient funds to pay the Broker for the <i>total amount</i>
+of his purchase, he is prohibited from buying <i>any</i> Raw Materials in that month.
+<br><br>
+<table class="smalltable">
+ <th></th>
+ <th colspan="3">JANUARY</th>
+<tr>
+ <td class="color" colspan="4">ORDERS</th>
+</tr>
+ <tr>
+   <td>Raw Materials</td>
+   <td>Units Bid</td>
+   <td>Bid Price</td>
+   <td>Amount Paid</td>
+ </tr>
+ <tr>
+   <td>X-FINE</td>
+   <td>2</td>
+   <td>40</td>
+   <td></td>
+ </tr>
+ <tr>
+   <td>FINE</td>
+   <td>4</td>
+   <td>35</td>
+   <td></td>
+ </tr>
+ <tr>
+   <td>STD.</td>
+   <td>6</td>
+   <td>23</td>
+   <td></td>
+ </tr>
+</table>
+<br>
+<i>Example: In a three-player game, player entered his January order as shown above.
+Total Units of X-FINE ordered by all players was 11, raising Market Price to $41.
+Total Units of FINE ordered was 15, bringing Market Price to $35.  Orders for
+STANDARD totaled 7, dropping Market Price to $17. Player adjusted his Tally and
+totaled his purchases as shown below.</i><br>
+<br>
+<table class="smalltable">
+ <th></th>
+ <th colspan="3">JANUARY</th>
+<tr>
+ <td class="color" colspan="4">ORDERS</th>
+</tr>
+ <tr>
+   <td>Raw Materials</td>
+   <td>Units Bid</td>
+   <td>Bid Price</td>
+   <td>Amount Paid</td>
+ </tr>
+ <tr>
+   <td>X-FINE</td>
+   <td><strike>2</strike></td>
+   <td><strike>40</strike></td>
+   <td>0</td>
+ </tr>
+ <tr>
+   <td>FINE</td>
+   <td>4</td>
+   <td>35</td>
+   <td>140</td>
+ </tr>
+ <tr>
+   <td>STD.</td>
+   <td>6</td>
+   <td>23</td>
+   <td>138</td>
+ </tr>
+</table>
+</div>
+<h3>2. MANUFACTORING.</h3>
+<div>
+After consulting the Productions Chart on the Price 
+Level Board, each player secretly decides on the amounts and kinds of Finished
+Goods he <i>intends to manufacture</i> from the Raw Materials he has on hand.
+<br><br>
+Three units of Raw Materials are required to manufacture one unit of Finished
+Goods as shown on Production Chart.  If a player is unable to supply the Raw
+Materials specified on the Chart, he may still manufacture his Goods by
+<i>substituting</i> an equal number of units of another Raw Material.  He
+may either substitute X-FINE for FINE or FINE for STANDARD; these are the
+only substitutions permitted.
+<br><br>
+<i>Example: Player wishes to manufacture a unit of "C".  Since he has only 1
+unit of STANDARD he substitutes 1 unit of FINE.  Thus, his "C" will consist
+of 2 FINE + 1 STANDARD.</i>
+<br><br>
+</div>
+<h3>3. SELLING THE FINISHED GOODS</h3>
+<div>
+After consulting the Price Level Board
+(<i>Opening Price</i> column and <i>Market Calculator</i>), each player enters
+his January offering of Finished Goods in the <i>Bid</i> columns of his Tally
+sheet - secretly recording <i>how many units</i> of each of the Finished Goods
+he wishes to sell and the <i>price per unit</i> he is willing to accept.  He
+may offer as many units of Finished Goods as he is able to manufacture.
+<br><br>
+After all players have entered their bids, Market Prices for the Finished Goods 
+are determined in the same manner as for <i>Purchasing Raw Material Certificates</i>
+except that Finished Goods section of Market Calculator is used.
+<br><br>
+<i>Example: In January, offerings of "A" total 7 units.  Market Calculator shows -3;
+since Opening Price of "A" is $140, January Market Price is $137.  No units of "B"
+were offered; Calculator shows +11 which is added to Opening Price of $115 for 
+January Market Price of $126.  Offerings of "C" totaled 5 units. Since Calculator
+shows +1 and Opening Price of "C" is $90, January Market Price is $91.</i>
+<br><br>
+After Market Prices have been posted, each player determines <i>which</i> Finished
+Goods he has succeeded in selling according to the following chart:
+<br><br>
+<table>
+ <th>PLAYER'S BID</th>
+ <th>GOODS SOLD</th>
+ <tr>
+  <td>Equal to or lower than Market Price</td>
+  <td>All Units at Bid Price</td>
+ </tr>
+ <tr>
+  <td>Higher than Market Price</td>
+  <td>No Units</td>
+ </tr>
+</table>
+<br><br>
+After adjusting their Tally sheets by <i>crossing out any overestimated bids</i>,
+players total their sales.  The Broker pays each player for units sold and collects
+the Raw Materials Certificates used in manufactoring the sold Goods.
+<br><br>
+If a player has made an error and cannot deliver the correct Raw Materials Certificates
+for all Finished Goods he has sold, he is prohibited from selling <i>any</i>
+Finished Goods in that month.
+<table class="smalltable">
+ <th></th>
+ <th colspan="3">JANUARY</th>
+<tr>
+ <td class="color" colspan="4">ORDERS</th>
+</tr>
+ <tr>
+   <td>Raw Materials</td>
+   <td>Units Bid</td>
+   <td>Bid Price</td>
+   <td>Amount Paid</td>
+ </tr>
+ <tr>
+   <td>X-FINE</td>
+   <td>5</td>
+   <td>42</td>
+   <td>210</td>
+ </tr>
+ <tr>
+   <td>FINE</td>
+   <td>4</td>
+   <td>36</td>
+   <td>144</td>
+ </tr>
+ <tr>
+   <td>STD.</td>
+   <td>3</td>
+   <td>15</td>
+   <td>45</td>
+ </tr>
+</table>
+<br><br>
+<i>Example: In three-player game, player entered January offering as shown above.
+Total units of "A" offered for sale was 4, raising Market Price to $143.  Total
+units of "B" was 1, bring Market Price to $124.  Offerings of "C" totaled 6,
+dropping Market Price to $89.  Player adjusted his Tally and totaled sales 
+as shown at right.</i>
+<br><br>
+<b>ALL OTHER MONTHS</b>.  Each month players purchase Raw Material Certificates,
+manufacture and sell the Finished Goods in the same manner as for January.  When
+determining current Market Prices, amounts shown on Market Calculator are added
+to or subtracted from previous month's Market Price.
+</div>  <!-- END 3. FINISHED GOODS -->
+</div>  <!-- END INSIDE PLAY ACCORDION -->
+</div>  <!-- END OUTSIDE PLAY ACCORDION -->
+<h2>ENDING THE GAME</h2>
+<div>
+When the number of months agreed upon has been played, players sell any unused
+Raw Material Certificates to the Broker at the final month's Market Price.  Player
+who has made the most profit for his corporation is the winner.
+</div>
+<h2>STRATEGY TIPS</h2>
+<div>
+Hi Clinton
+</div>
+</div>  <!-- END MAIN  ACCORDION -->
+<br><br><br>
+
+
